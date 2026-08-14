5f9e96b4580e39a5114c0e7c1e0dc949fa9ba1cb
Webtactics <paul@webtactics.com.au>
2026-08-14 14:57:17 +1000

    cms

--- admin/preview-templates/index.js
+++ admin/preview-templates/index.js
@@ -3,7 +3,10 @@ import Page from "/admin/preview-templates/page.js";
 
 // Register the Post component as the preview for entries in the blog collection
 CMS.registerPreviewTemplate("blog", Post);
-CMS.registerPreviewTemplate("pages", Page);
+
+// Every "pages_*" collection (pages/, pages/about, pages/contact, etc.) shares the same Page preview
+["pages", "pages_about", "pages_contact", "pages_learning_environments", "pages_members", "pages_parents_information"]
+  .forEach(name => CMS.registerPreviewTemplate(name, Page));
 
 CMS.registerPreviewStyle("/_includes/assets/css/inline.css");
 // Register any CSS file on the home page as a preview style
